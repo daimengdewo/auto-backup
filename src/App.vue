7b12e927c0daa1932e9caec8fb7c8192df630189
daimengdewo <915523005@qq.com>
2024-01-16 00:04:27 +0800

    init-2024-01-16

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Demo from '../src/views/test/Demo.vue'
+</script>
+
+<template>
+    <div class="wrapper">
+      <Demo></Demo>
+    </div>
+</template>
+
+<style lang="css">
+
+</style>
---
+++ src/views/test/Demo.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import {ref,reactive} from 'vue'
+    const count = ref(0)
+    const handleClick = () => {
+        count.value++
+    }
+    const raw = ref('<h1>测试</h1>')
+    const object = ref({
+        id: '001',
+        class: 'box'
+    })
+</script>
+
+<template>
+    <div>
+        {{count}}
+        <button @click="handleClick">ADD</button>
+    </div>
+    <div v-html="raw"></div>
+    <div :="object">object</div>
+</template>
+
+<style lang="css">
+
+</style>

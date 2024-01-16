afbd9c9b6bbbd975091cc1fa1e0db7c69609aeb8
daimengdewo <915523005@qq.com>
2024-01-16 18:12:34 +0800

    2024-01-16

--- src/views/test/Demo.vue
+++ src/views/test/Demo.vue
@@ -1,23 +1,14 @@
 <script setup>
     import {ref,reactive} from 'vue'
-    const count = ref(0)
-    const handleClick = () => {
-        count.value++
-    }
-    const raw = ref('<h1>测试</h1>')
-    const object = ref({
-        id: '001',
-        class: 'box'
-    })
+    const username = ref("")
 </script>
 
 <template>
-    <div>
-        {{count}}
-        <button @click="handleClick">ADD</button>
-    </div>
-    <div v-html="raw"></div>
-    <div :="object">object</div>
+    <ul>
+        <li>
+            <input type="text" v-model="username">
+        </li>
+    </ul>
 </template>
 
 <style lang="css">
